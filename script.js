1b2160433669c06b2cb9f6384ab3149c97840dac
ayo <ayomidebasit026@gmail.com>
2022-10-20 14:07:40 -0700

    Completed home page

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const searchBtn = document.querySelector('.search-icon-container');
 const searchBar = document.querySelector('.search-bar');
 
 let sideMenuFlag = false;
+let searchBarFlag = false;
 let navHeight = `${navBar.getBoundingClientRect().height}px`
+const body = document.body;
 
 toggleBtn.addEventListener('click', function () {
     if (!sideMenuFlag) {
@@ -13,22 +15,37 @@ toggleBtn.addEventListener('click', function () {
         sideMenu.style.top = navHeight;
         sideMenu.classList.add('display-side-menu');
         toggleBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        body.style.overflow = 'hidden';
     } else {
         sideMenuFlag = false;
         sideMenu.classList.remove('display-side-menu');
-        toggleBtn.innerHTML = `<svg width="15" height="2" viewBox="0 0 18 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 1H17" stroke="#EFEEE0" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" />
-                                </svg>
-                                <svg width="15" height="2" viewBox="0 0 18 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M1 1H17" stroke="#EFEEE0" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" />
-                                </svg>`
+        toggleBtn.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_107_495)">
+        <path d="M4 8H20" stroke="#EFEEE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M4 16H20" stroke="#EFEEE0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_107_495">
+        <rect width="24" height="24" fill="white"/>
+        </clipPath>
+        </defs>
+        </svg>
+        `
+        body.style.overflow = 'scroll';
     }
     console.log(navHeight);
 })
 
 searchBtn.addEventListener('click', function () {
-    searchBar.style.top = navHeight;
-    searchBar.classList.toggle('display-search-bar');
+    if (!searchBarFlag) {
+        searchBarFlag = true;
+        searchBar.style.top = navHeight;
+        searchBar.classList.add('display-search-bar');
+        body.style.overflow = 'hidden';
+    } else {
+        searchBarFlag = false;
+        searchBar.classList.remove('display-search-bar');
+        body.style.overflow = 'scroll';
+        
+    }
 })
